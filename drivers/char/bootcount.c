450fc39da07177bcde4f259bb3c5f61f32f95ba0
Daniel Cassidy <mail@danielcassidy.me.uk>
2026-01-09 13:14:49 +0000

    bootcount: delete redundant include

--- drivers/char/bootcount.c
+++ drivers/char/bootcount.c
@@ -27,7 +27,6 @@
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/ptrace.h>
-#include <linux/uaccess.h>
 
 #define	UBOOT_BOOTCOUNT_MAGIC_OFFSET	0x04	/* offset of magic number */
 #define	UBOOT_BOOTCOUNT_MAGIC		0xB001C041 /* magic number value */
